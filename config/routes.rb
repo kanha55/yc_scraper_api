f5735cf109ce277ee4267ae90bddc94f29766e34
Kanhaiya <kanhu.dubey9@gmail.com>
2024-07-24 16:09:09 +0530

    Refactor routes.rb to use Rails.application.routes.draw

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
-namespace :api do
-  namespace :v1 do
-    resources :companies, only: [:index]
+Rails.application.routes.draw do
+  namespace :api do
+    namespace :v1 do
+      resources :companies, only: [:index]
+    end
   end
 end
